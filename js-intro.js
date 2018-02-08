fae25779d470b15e07a97cf08e5072ab8f8f4ee7
suuuth <suther98@msu.edu>
2018-02-08 10:37:40 -0500

    first commit of js-intro project

---
+++ js-intro.js
@@ -0,0 +1,45 @@
+var hat = new Audio('hihat.wav');
+var snare = new Audio('snare.wav');
+var kick = new Audio('kick.wav');
+var ride = new Audio('ride.wav');
+var fTom = new Audio('boom.wav');
+var openhat = new Audio('openhat.wav');
+var tink = new Audio('tink.wav');
+var clap = new Audio('clap.wav');
+var tom = new Audio('tom.wav');
+
+function playHat() {
+  hat.play();
+}
+
+function playSnare() {
+  snare.play();
+}
+
+function playKick() {
+  kick.play();
+}
+
+function playRide() {
+  ride.play();
+}
+
+function playfTom() {
+  fTom.play();
+}
+
+function playOpenhat() {
+  openhat.play();
+}
+
+function playTink() {
+  tink.play();
+}
+
+function playClap() {
+  clap.play();
+}
+
+function playTom() {
+  tom.play();
+}
